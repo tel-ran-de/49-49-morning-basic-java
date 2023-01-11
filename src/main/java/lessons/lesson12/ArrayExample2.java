9b288f9cb6c721fb9e5c916f24b311654ee0d619
StanislavPB <perminovsb@gmail.com>
2023-01-11 15:10:18 +0200

    add: lesson 12 lessoncode

--- src/main/java/lessons/lesson12/ArrayExample2.java
+++ src/main/java/lessons/lesson12/ArrayExample2.java
@@ -19,18 +19,20 @@ public class ArrayExample2 {
         System.out.println(array[0]==array[2]);
 
         String [] stringArray = new String[4];
-        stringArray[0] =" word 1";
+        stringArray[0] = "word 1";
         stringArray[1] = "word 2c ";
-        stringArray[2] = "word 1";
+        stringArray[2] = " word 1 ";
         stringArray[3] = "word 4 ";
+
+        stringArray[2]= stringArray[2].trim();
 
         System.out.println(Arrays.toString(stringArray));
         System.out.println(stringArray[0]+stringArray[1]+stringArray[2]+stringArray[3]);
 
+        System.out.println(stringArray[0]==stringArray[1]);
+        System.out.println(stringArray[0]==stringArray[2]);
+
         System.out.println(stringArray[0].equals(stringArray[1]));
         System.out.println(stringArray[0].equals(stringArray[2]));
-
-        
-
     }
 }
